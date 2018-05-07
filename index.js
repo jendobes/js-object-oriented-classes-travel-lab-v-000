2d742cf60a99bbb01299c574e3f3458d3cecd37e
Jennifer Dobak <jdobak1@gmail.com>
2018-05-07 15:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Route {
   }
 
   blocksTravelled() {
-
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
   }
 
   estimatedTime() {
