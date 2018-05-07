a0793230ee5f7ff5ee189dc4d9b80fbd3793c237
Jennifer Dobak <jdobak1@gmail.com>
2018-05-07 15:10:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+class Driver {
+  constructor(name, startDate) {
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+}
